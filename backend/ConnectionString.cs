05220df1f2304b9672e8055e7b0f5a9c82cb4b3e
Shaheer <103843506+shaheerzk01@users.noreply.github.com>
2023-07-25 05:55:29 -0700

    Add files via upload

---
+++ backend/ConnectionString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Lab6
+{
+	public static class ConnectionString
+	{
+		public static SqlConnection mySqlConnection;
+
+		public static SqlConnection GetConnection()
+		{
+			if(mySqlConnection == null)
+			{
+				mySqlConnection = new SqlConnection();
+				mySqlConnection.ConnectionString = @"Data Source=localhost,1433;Initial Catalog=ATTENDENCE_SYSTEM;User Id=sa;Password=reallyStrongPwd123";
+				mySqlConnection.Open();
+			}
+			return mySqlConnection;
+		}
+	}
+
+	
+}
+
